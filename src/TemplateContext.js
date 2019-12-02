143d2a808245f426e6941909e9e220f919caeddc
Eugene Cheah <eugeneqin@gmail.com>
2019-12-02 22:59:25 +0800

    Added outputRemap option

--- src/TemplateContext.js
+++ src/TemplateContext.js
@@ -83,6 +83,63 @@ function scanAndApplyFileTemplates( input, output, templatePath, cgCtx ) {
 }
 
 /**
+ * Given the configured output, remap it accordingly
+ * 
+ * @param {Object} output 
+ * @param {Object} templateOutput 
+ * @param {*} outputRemap 
+ */
+function processOutputRemap( output, templateOutput, outputRemap ) {
+	//
+	// Collapse output, into a single string - if configured
+	//
+	if( typeof outputRemap == "string" ) {
+		// Prepare the final output str
+		let outputStrArr = [];
+
+		// By joining the various key values
+		let keys = Object.keys(templateOutput).sort();
+		for( subKey of keys ) {
+			outputStrArr.push( templateOutput[subKey] );
+		}
+
+		// And set the output
+		output[outputRemap] = outputStrArr.join("\n");
+		return;
+	}
+
+	//
+	// Assumes an object otherwise, and map the keys to array/keys
+	//
+	for( outputKey of Object.keys(outputRemap).sort() ) {
+		// Get the output setting
+		let outputSet = outputRemap[outputKey];
+
+		// For the string set, do a direct remap
+		if( typeof outputSet == "string" ) {
+			output[ outputKey ] = templateOutput[ outputSet ];
+			continue;
+		}
+
+		// For the array, iterate and join them
+		if( Array.isArray( outputSet ) ) {
+			let outputStrArr = [];
+			for( subKey of outputSet ) {
+				outputStrArr.push( templateOutput[subKey] );
+			}
+			output[ outputKey ] = outputStrArr.join("\n");
+			continue;
+		}
+
+		// Unknown output setting type
+		throw "[FATAL ERROR]: Unknown outputRemap setting : "+JSON.stringify(outputRemap);
+	}
+
+	// rreturn processed output
+	return output;
+}
+
+/**
  * Apply the template function json config, if provided
  * 
  * @param {Object} input 
@@ -122,8 +179,15 @@ function applyTemplateJSON_noRecursion( input, output, templatePath, cgCtx ) {
 			return;
 		}
 
-		// Apply the template
-		cgCtx.applyTemplate( tObj.template, tObj.input, output );
+		// Output remapping support
+		if( tObj.outputRemap ) {
+			// Generate the output
+			let templateOutput = cgCtx.applyTemplate( tObj.template, tObj.input, {} );
+			processOutputRemap( output, templateOutput, tObj.outputRemap );
+		} else {
+			// Apply the template directly
+			cgCtx.applyTemplate( tObj.template, tObj.input, output );
+		}
 	}
 
 	// If its an array iterate it
