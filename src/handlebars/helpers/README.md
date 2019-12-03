4fa87d0c8bd62518cbaeecaa9b778c4bf373c1ba
Eugene Cheah <eugeneqin@gmail.com>
2019-12-03 11:08:09 +0800

    handlebarsParse rename

---
+++ src/handlebars/helpers/README.md
@@ -0,0 +1,4 @@
+# handlebar/helpers
+
+The following are added _in addition to_ the ones listed in `handlebars-helpers` library - https://github.com/helpers/handlebars-helpers
+
---
+++ src/handlebars/helpers/json2yaml.js
@@ -0,0 +1,82 @@
+/**
+ * Given a JSON object, stringify it into a yaml format.
+ * This is designed to be used in a larger yaml template
+ * 
+ * For Examples:
+ * 
+ * input.json
+ * ```
+ * {
+ * 	"hello": {
+ * 		"world": [1,2]
+ * 	}
+ * }
+ * ```
+ * 
+ * template.yaml
+ * ```
+ * hello:
+ *   {{#json2yaml "hello.world"}}{{/json2yaml}}
+ * ```
+ * 
+ * output.yaml
+ * ```
+ * hello:
+ *   - 1
+ *   - 2
+ * ```
+ */
+const json2yaml = require("json2yaml");
+handlebars.registerHelper('json2yaml', function() {
+	// Get the arguments
+	// note the last argument is the "handlebar context"
+	const rawArgs = Array.prototype.slice.call(arguments);
+	const args  = rawArgs.slice(0, -1);
+	const hbCtx = rawArgs[ rawArgs.length - 1 ];
+
+	// Side note:
+	// this -> is equivalent to the hbCtx.data
+	
+	// Get the input path (to process)
+	let inputPath = args[0] || hbCtx.fn(this);
+
+	// Fetch the json object
+	let jsonObj = getNested( this, inputPath );
+	if( jsonObj == null ) {
+		throw "Unexpected blank json for json2yaml - path: "+inputPath;
+	}
+
+	//----------------------------------------------
+	// Get the yaml str
+	//----------------------------------------------
+
+	let yamlRawStr = json2yaml.stringify(jsonObj);
+
+	//----------------------------------------------
+	// Lets do some processing on the yaml string
+	//----------------------------------------------
+
+	// white space indenting prep
+	let whitespaceCount = hbCtx.loc.start.column;
+	let whitespaceStr = "";
+	for(let i=0; i<whitespaceCount; ++i) {
+		whitespaceStr += " ";
+	}
+
+	// Split the resulting yaml
+	let yamlStrArr = yamlRawStr.split("\n").slice(1, -1);
+	let finalYamlStrArr = [];
+
+	// And process each line
+	for(let i=0; i<yamlStrArr.length; ++i) {
+		// Skip additional whitespace for first line
+		if( i == 0 ) {
+			finalYamlStrArr[i] = yamlStrArr[i].slice(2);
+		} else {
+			finalYamlStrArr[i] = whitespaceStr + yamlStrArr[i].slice(2);
+		}
+	}
+
+	// Lets join it up - and return the value
+	return finalYamlStrArr.join("\n");
+});
