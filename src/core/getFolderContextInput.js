a58e128c7198d8336eb4b0f1cb6114110595f67f
Eugene Cheah <eugeneqin@gmail.com>
2019-12-03 16:03:57 +0800

    Folder context input function

---
+++ src/core/getFolderContextInput.js
@@ -0,0 +1,50 @@
+//
+// Constant dependencies
+//
+const path            = require("path");
+const hjsonParse      = requrie("./../conv/hjsonParse")
+const handlebarsParse = require("../handlebars/handlebarsParse")
+const jsonObjectClone = requrie("./../conv/jsonObjectClone")
+const nestedObjAssign = require("./../struct/nestedObjAssign")
+
+/**
+ * Scans the given folder path for input settings, and return it combined with the baseInput
+ * 
+ * @param {String}   folderPath to scan for various input settings
+ * @param {Object}   baseInput to combined with (note this is not modified)
+ * @param {Function} cgCtxFunc to setup, and return configami context, given the current input
+ * 
+ * @return {Object} final input object
+ */
+module.exports = function getFolderContextInput( folderPath, baseInput, cgCtxFunc ) {
+
+	// Prepare the base return object
+	let ret = jsonObjectClone( baseInput || {} );
+
+	// HJSON input files to support (if valid)
+	const hjsonInputList = [ "input.configami.json", "input.configami.hjson" ]
+
+	// Lets scan and process various handlebars json
+	for( const fileName of hjsonInputList ) {
+		const filePath = path.join( folderPath, fileName );
+		ret = nestedObjAssign(ret, handlebarsParse.hjsonFile(filePath, ret));
+	}
+
+	// Lets parse the handlebars js file
+	const jsFilePath = path.join( folderPath, "input.configami.js" );
+	if( fsh.isFile( jsFilePath ) ) {
+		// Load the js module
+		const inputMod = require( jsFilePath );
+		if( inputMod ) {
+			// Get the configami context
+			const cgCtx = cgCtxFunc(ret);
+
+			// Executed the js module
+			ret = inputMod( cgCtx, ret ) || ret;
+			cgCtx.input = ret;
+		}
+	}
+
+	// Final return object
+	return ret;
+}
